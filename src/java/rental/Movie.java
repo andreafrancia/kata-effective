d55c419f0619c74185e023482b3be0c08bf9c76b
Andrea Francia <andrea@andreafrancia.it>
2019-09-05 21:24:14 +0200

    First import

---
+++ src/java/rental/Movie.java
@@ -0,0 +1,45 @@
+package rental;
+
+public class Movie {
+
+    public enum Type {
+        REGULAR, NEW_RELEASE, CHILDREN, UNKNOWN;
+    }
+
+    private String title;
+    Price price;
+
+    public Movie(String title, Type priceCode) {
+        this.title = title;
+        setPriceCode(priceCode);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setPriceCode(Type priceCode) {
+        switch (priceCode) {
+            case CHILDREN:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "invalid price code");
+        }
+    }
+
+    public double getCharge(int daysRented) {
+        return price.getCharge(daysRented);
+    }
+
+    public int getPoints(int daysRented) {
+        return price.getPoints(daysRented);
+    }
+}
